78671048784b53e60b29f3ed3783193113aeae97
Lars Frogner <lars_frogner@hotmail.com>
2019-02-21 19:42:25 +0100

    Update shader

--- shaders/simple.fragment.glsl
+++ shaders/simple.fragment.glsl
@@ -3,14 +3,12 @@
 in vec3 ex_tex_coord;
 out vec4 out_color;
 
-uniform sampler3D texture0;
+uniform sampler3D texture_0;
+uniform sampler1D transfer_function_0;
 
 void main(void)
 {
-    float intensity;
-    intensity = texture(texture0, ex_tex_coord).r;
-    out_color.x = intensity;
-    out_color.y = intensity;
-    out_color.z = intensity;
-    out_color.w = intensity;
+    float field_value;
+    field_value = texture(texture_0, ex_tex_coord).r;
+    out_color = texture(transfer_function_0, field_value);
 }
