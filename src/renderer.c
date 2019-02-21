73fd393119b4b26851389bd0433300e5708d8d43
Lars Frogner <lars_frogner@hotmail.com>
2019-02-21 19:43:12 +0100

    Include transfer functions

--- src/renderer.c
+++ src/renderer.c
@@ -5,7 +5,9 @@
 #include "fields.h"
 #include "shaders.h"
 #include "transformation.h"
-#include "textures.h"
+#include "texture.h"
+#include "field_textures.h"
+#include "transfer_functions.h"
 #include "axis_aligned_planes.h"
 
 
@@ -33,11 +35,22 @@ void initialize_renderer(void)
 
     Field field = read_bifrost_field("/Users/larsfrog/Code/output_visualization/no_ebeam/en024031_emer3.0sml_orig_631_tg_lowres.raw",
                                      "/Users/larsfrog/Code/output_visualization/no_ebeam/en024031_emer3.0sml_orig_631_tg_lowres.dat");
-    add_scalar_field_texture(&field);
+
+    const unsigned int texture_id = add_scalar_field_texture(&field);
+
+    const unsigned int TF_id = add_transfer_function();
+    //add_piecewise_linear_transfer_function_node(TF_id, TF_ALPHA, 0, 0);
+    //add_piecewise_linear_transfer_function_node(TF_id, TF_ALPHA, 1, 1);
+    const float low = 0.0f;
+    const float high = 0.2f;
+    set_logarithmic_transfer_function(TF_id, TF_RED, low, high, 0, 1);
+    set_logarithmic_transfer_function(TF_id, TF_GREEN, low, high, 0, 1);
+    set_logarithmic_transfer_function(TF_id, TF_BLUE, low, high, 0, 1);
+    set_logarithmic_transfer_function(TF_id, TF_ALPHA, low, high, 0, 1);
 
     create_shader_program(&shader_program);
 
-    create_planes(&field, 1.0f);
+    create_planes(field.size_x, field.size_y, field.size_z, field.extent_x, field.extent_y, field.extent_z, 0.5f);
 
     create_transform_matrices(2.0f, 60.0f, (float)window_shape.width/window_shape.height, 0.01f, 100.0f);
 
@@ -47,6 +60,8 @@ void initialize_renderer(void)
     load_planes();
     load_textures(&shader_program);
     load_transform_matrices(&shader_program);
+
+    print_transfer_function(TF_id, TF_ALPHA);
 }
 
 void sync_renderer(void)
@@ -58,7 +73,8 @@ void sync_renderer(void)
 void cleanup_renderer(void)
 {
     cleanup_planes();
-    cleanup_textures();
+    cleanup_field_textures();
+    cleanup_transfer_functions();
     destroy_shader_program(&shader_program);
 }
 
@@ -75,6 +91,8 @@ void update_renderer_window_size_in_pixels(int width, int height)
 
 void renderer_update_callback(void)
 {
+    sync_transfer_functions();
+
     glClear(GL_COLOR_BUFFER_BIT);
 
     glUseProgram(shader_program.id);
