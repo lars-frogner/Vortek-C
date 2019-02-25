6e055460a8cb9681e9f61516904d08b24d337458
Lars Frogner <lars_frogner@hotmail.com>
2019-02-25 19:35:38 +0100

    Use initial window resolution relative to screen resolution

--- src/window.c
+++ src/window.c
@@ -11,8 +11,8 @@
 
 
 #define WINDOW_TITLE "Vortek"
-#define DEFAULT_WINDOW_WIDTH 600
-#define DEFAULT_WINDOW_HEIGHT 600
+#define DEFAULT_WINDOW_HEIGHT_FRACTION 0.6f
+#define DEFAULT_WINDOW_ASPECT_RATIO 1.0f
 
 
 typedef struct Window
@@ -30,6 +30,7 @@ typedef struct FPSCounter
 } FPSCounter;
 
 
+static void get_screen_resolution(int* width, int* height);
 static void update_FPS(GLFWwindow* window_handle);
 static void error_callback(int error, const char* description);
 static void framebuffer_size_callback(GLFWwindow* window_handle, int width, int height);
@@ -59,20 +60,18 @@ void initialize_window(void)
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-    window.handle = glfwCreateWindow(DEFAULT_WINDOW_WIDTH, DEFAULT_WINDOW_HEIGHT, WINDOW_TITLE, NULL, NULL);
+    int screen_width, screen_height;
+    get_screen_resolution(&screen_width, &screen_height);
+
+    window.handle = glfwCreateWindow((int)(screen_height*DEFAULT_WINDOW_HEIGHT_FRACTION*DEFAULT_WINDOW_ASPECT_RATIO),
+                                     (int)(screen_height*DEFAULT_WINDOW_HEIGHT_FRACTION),
+                                     WINDOW_TITLE, NULL, NULL);
 
     if (!window.handle)
     {
         glfwTerminate();
         print_severe_message("Could not create window.");
     }
-
-    glfwSetFramebufferSizeCallback(window.handle, framebuffer_size_callback);
-    glfwSetWindowSizeCallback(window.handle, window_size_callback);
-    glfwSetKeyCallback(window.handle, keyboard_callback);
-    glfwSetMouseButtonCallback(window.handle, mouse_button_callback);
-    glfwSetCursorPosCallback(window.handle, cursor_pos_callback);
-    glfwSetScrollCallback(window.handle, scroll_callback);
 
     glfwMakeContextCurrent(window.handle);
 
@@ -84,9 +83,17 @@ void initialize_window(void)
     glfwGetFramebufferSize(window.handle, &current_window_width, &current_window_height);
     check(current_window_width > 0);
     check(current_window_height > 0);
+
     update_renderer_window_size_in_pixels(current_window_width, current_window_height);
 
     glfwSwapInterval(1);
+
+    glfwSetFramebufferSizeCallback(window.handle, framebuffer_size_callback);
+    glfwSetWindowSizeCallback(window.handle, window_size_callback);
+    glfwSetKeyCallback(window.handle, keyboard_callback);
+    glfwSetMouseButtonCallback(window.handle, mouse_button_callback);
+    glfwSetCursorPosCallback(window.handle, cursor_pos_callback);
+    glfwSetScrollCallback(window.handle, scroll_callback);
 }
 
 void mainloop(void)
@@ -110,6 +117,14 @@ void cleanup_window(void)
     check(window.handle);
     glfwDestroyWindow(window.handle);
     glfwTerminate();
+}
+
+static void get_screen_resolution(int* width, int* height)
+{
+    const GLFWvidmode* mode = glfwGetVideoMode(glfwGetPrimaryMonitor());
+
+    *width = mode->width;
+    *height = mode->height;
 }
 
 static void update_FPS(GLFWwindow* window_handle)
@@ -171,7 +186,7 @@ static void mouse_button_callback(GLFWwindow* window_handle, int button, int act
         {
             double x_click_pos, y_click_pos;
             glfwGetCursorPos(window_handle, &x_click_pos, &y_click_pos);
-            trackball_leftclick_callback(x_click_pos, y_click_pos, window.height_screen_coords);
+            trackball_leftclick_callback(x_click_pos, y_click_pos, window.width_screen_coords, window.height_screen_coords);
 
             mouse_is_pressed = 1;
         }
@@ -186,7 +201,7 @@ static void cursor_pos_callback(GLFWwindow* window_handle, double xpos, double y
 {
     if (mouse_is_pressed)
     {
-        trackball_mouse_drag_callback(xpos, ypos, window.height_screen_coords);
+        trackball_mouse_drag_callback(xpos, ypos, window.width_screen_coords, window.height_screen_coords);
     }
 }
 
