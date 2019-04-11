10d7cb26e0ac72a62900a606c9abbb14d98d15ab
Lars Frogner <lars_frogner@hotmail.com>
2019-04-11 09:44:14 +0200

    Add border around matplotlib canvas

--- gui/matplotlib_canvas.py
+++ gui/matplotlib_canvas.py
@@ -7,9 +7,9 @@ from matplotlib.figure import Figure
 
 class MatplotlibCanvas(FigureCanvas):
 
-    def __init__(self, parent=None, width=5, height=4, dpi=100):
+    def __init__(self, parent=None, width=6, height=4, dpi=100, edgecolor='k', linewidth=1.0, tight_layout=True):
 
-        self.fig = Figure(figsize=(width, height), dpi=dpi)
+        self.fig = Figure(figsize=(width, height), dpi=dpi, edgecolor=edgecolor, linewidth=linewidth, tight_layout=tight_layout)
         self.axes = self.fig.add_subplot(111)
 
         FigureCanvas.__init__(self, self.fig)
